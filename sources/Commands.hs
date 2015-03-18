72769edcab04f311f728e2889c02f03442cd613d
Sam Boosalis <samboosalis@gmail.com>
2015-03-17 23:23:12 -0700

    module Commands re-exports everything

---
+++ sources/Commands.hs
@@ -0,0 +1,48 @@
+module Commands
+ ( module Commands.Etc
+ -- , module Commands.Instances
+
+ , module Commands.Command.Combinator
+ , module Commands.Command.Sugar
+ -- , module Commands.Command.Types
+ , module Commands.Command
+
+ , module Commands.Frontends.Dragon13.Optimize
+ , module Commands.Frontends.Dragon13.Render
+ , module Commands.Frontends.Dragon13.Text
+ , module Commands.Frontends.Dragon13.Types
+ , module Commands.Frontends.Dragon13
+
+ , module Commands.Grammar.Types
+ , module Commands.Grammar
+
+ , module Commands.Graph
+
+ , module Commands.Munging
+
+ , module Commands.Parse.Types
+ , module Commands.Parse
+ , module Commands.Parsec
+
+ , module Control.Alternative.Free.Tree
+ ) where
+
+import Commands.Etc
+import Commands.Instances ()
+import Commands.Command.Combinator
+import Commands.Command.Sugar
+-- import Commands.Command.Types
+import Commands.Command
+import Commands.Frontends.Dragon13.Optimize
+import Commands.Frontends.Dragon13.Render
+import Commands.Frontends.Dragon13.Text
+import Commands.Frontends.Dragon13.Types
+import Commands.Frontends.Dragon13
+import Commands.Grammar.Types
+import Commands.Grammar
+import Commands.Graph
+import Commands.Munging
+import Commands.Parse.Types
+import Commands.Parse
+import Commands.Parsec hiding (optional,lower,runParser)
+import Control.Alternative.Free.Tree hiding  (Empty)

77691eee63129a2f5c5d28c1fe175835d4da4e8f
Sam Boosalis <samboosalis@gmail.com>
2015-07-10 20:21:42 -0700

    DocTest.Discover

---
+++ tests/Test/DocTest/Discover.hs
@@ -0,0 +1,44 @@
+{-# LANGUAGE LambdaCase #-}
+module Test.DocTest.Discover where
+
+import CabalLenses.PackageDescription
+import Control.Lens
+import Distribution.InstalledPackageInfo     (PError)
+import Distribution.ModuleName
+import Distribution.PackageDescription
+import Distribution.PackageDescription.Parse (ParseResult (..),
+                                              parsePackageDescription)
+
+import Control.Arrow                         ((>>>))
+import Data.List                             (intercalate)
+import Data.Monoid                           ((<>))
+
+
+loadPackageDescription :: IO (Either PError GenericPackageDescription)
+loadPackageDescription = do
+ projectDirectory <- return "/Users/sambo/voice/commands-core/commands-core.cabal"
+ cabalFile <- readFile projectDirectory
+ return$ case parsePackageDescription cabalFile of
+  ParseFailed e -> Left e
+  ParseOk _ pkg -> Right pkg
+
+-- | when `hs-source-dirs` is not singleton, includes nonexistent files.
+modulePaths :: GenericPackageDescription -> [FilePath]
+modulePaths pkg = do
+ d <- pkg^..packageHsSourcesDirs
+ m <- pkg^..packageExposedModules
+ return$ intercalate "/" (d : components m) -- e.g. ("sources/sources" : ["Commands","Core"])
+
+-- packageLibrary :: Traversal' GenericPackageDescription Library
+-- packageLibrary = packageDescriptionL.libraryL.each
+
+-- | ignores 'condTreeComponentsL'
+packageCondLibrary :: Traversal' GenericPackageDescription Library
+packageCondLibrary = condLibraryL.each.condTreeDataL
+
+packageExposedModules :: Traversal' GenericPackageDescription ModuleName
+packageExposedModules = packageCondLibrary.exposedModulesL.each
+
+packageHsSourcesDirs :: Traversal' GenericPackageDescription FilePath
+packageHsSourcesDirs = packageCondLibrary.libBuildInfoL.hsSourceDirsL.each
+
--- tests/Tests.hs
+++ tests/Tests.hs
@@ -1,101 +1,32 @@
 {-# LANGUAGE LambdaCase #-}
 module Main where
 import Commands.Test.Properties
-import Data.Monoid              ((<>))
+import Test.DocTest.Discover
+
 import Test.DocTest
 import Test.Tasty
 import Test.Tasty.QuickCheck
 
+import Data.Monoid              ((<>))
+import System.Exit              (exitFailure)
 
-tastytest = defaultMain $ testGroup "QuickCheck"
+
+tastytests = defaultMain $ testGroup "QuickCheck"
  [ testProperty "serialized DNSGrammar is valid Python" prop_DNSGrammar
  ]
 
+-- doctestModules = fmap ("sources/" <>) . (fmap.fmap) (\case '.' -> '/'; c -> c) $
 
-{- object file needed because:
-
-@
-ByteCodeLink: can't find label
-During interactive linking, GHCi couldn't find the following symbol:
-  currentApplicationPath
-This may be due to you not asking GHCi to load extra object files,
-archives or DLLs needed by your current session.
-@
-
-after adding @"dist/build/cbits/objc_actor.o"@, still doesn't work:
-
-@
-ghc: panic! (the 'impossible' happened)
-  (GHC version 7.8.3 for x86_64-apple-darwin):
-  Loading temp shared object failed: dlopen(/var/folders/2z/3c9t_c2d22175v217mf5znsh0000gn/T/ghc96820_0/ghc96820_1.dylib, 9): Symbol not found: _NSStringPboardType
-  Referenced from: /var/folders/2z/3c9t_c2d22175v217mf5znsh0000gn/T/ghc96820_0/ghc96820_1.dylib
-  Expected in: flat namespace
- in /var/folders/2z/3c9t_c2d22175v217mf5znsh0000gn/T/ghc96820_0/ghc96820_1.dylib
-@
-
-even with: @["-frameworkCocoa", "-optl-ObjC"]@
-
--}
-doctestArgs = [ "-frameworkCocoa", "-optl-ObjC", "dist/build/cbits/objc_actor.o" ] <> modules
-
-modules = fmap ("sources/" <>) . (fmap.fmap) (\case '.' -> '/'; c -> c) $
-
-  [ "Commands.Core"
-
-  , "Commands.Symbol"
-  , "Commands.Symbol.Types"
-
-  , "Commands.Grammar.Types"
-  , "Commands.Grammar"
-
-  , "Commands.Parse.Types"
-  , "Commands.Parse"
-  , "Commands.Parsec"
-
-  , "Commands.Parsers.Earley"
-
-  , "Commands.Frontends.Dragon13"
-  , "Commands.Frontends.Dragon13.Types"
-  , "Commands.Frontends.Dragon13.Lens"
-  , "Commands.Frontends.Dragon13.Text"
-  , "Commands.Frontends.Dragon13.Render"
-  , "Commands.Frontends.Dragon13.Optimize"
-  , "Commands.Frontends.Dragon13.Shim"
-  , "Commands.Frontends.Dragon13.Serialize"
-
-  , "Commands.Backends.OSX"
-  , "Commands.Backends.OSX.Example"
-  , "Commands.Backends.OSX.Types"
-  , "Commands.Backends.OSX.Bindings"
-  , "Commands.Backends.OSX.Bindings.Raw"
-  , "Commands.Backends.OSX.Constants"
-  , "Commands.Backends.OSX.Marshall"
-  , "Commands.Backends.OSX.Execute"
-  , "Commands.Backends.OSX.DSL"
-
-  , "Commands.Servers.Servant"
-  , "Commands.Servers.Servant.Types"
-  , "Commands.Servers.Servant.API"
-
-  , "Commands.DNS13OSX9"
-  , "Commands.DNS13OSX9.Types"
-  , "Commands.DNS13OSX9.Primitive"
-  , "Commands.DNS13OSX9.Combinator"
-
-  , "Commands.Plugins.Example"
-  , "Commands.Plugins.Example.Phrase"
-
-  , "Commands.Sugar"
-  , "Commands.Sugar.Press"
-
-  , "Commands.Etc"
-  , "Commands.Etc.Generics"
-  , "Commands.Instances"
-  , "Commands.Munging"
-  , "Commands.Graph"
-  , "Control.Alternative.Free.Associated"
-  ]
+doctests = do
+ loadPackageDescription >>= \case
+  Left e -> do
+   print e
+   exitFailure
+  Right pkg -> do
+   -- print (pkg)
+   print (modulePaths pkg)
+   doctest (modulePaths pkg)
 
 main = do
- -- doctest doctestArgs
- tastytest
+ -- doctests  -- ByteCodeLink: can't find label. During interactive linking, GHCi couldn't find the following symbol: currentApplicationPath
+ tastytests

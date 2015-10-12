f4db08e5a781b11db95f8fb615d36d8af65dd8b3
Sam Boosalis <samboosalis@gmail.com>
2015-10-12 12:45:30 -0700

    rm  Data.Enumerate

--- sources/Commands/Main.hs
+++ sources/Commands/Main.hs
@@ -2,13 +2,11 @@
 {-# OPTIONS_GHC -fno-warn-missing-signatures #-}
 module Commands.Main where 
 
-import Data.Canonical 
-
 import           System.Environment             (getArgs)
 
 
 main = mainWith =<< getArgs
 
 mainWith = \case
- _ -> mainCanonical 
+ _ -> return() 
 

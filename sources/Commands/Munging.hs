ed84c11a16da644f3a1654b726fa60a71f3813be
Sam Boosalis <samboosalis@gmail.com>
2015-11-12 15:24:03 -0800

    fix unClassCase

--- sources/Commands/Munging.hs
+++ sources/Commands/Munging.hs
@@ -1,3 +1,11 @@
+{- | 
+
+> -- for doctests 
+>>> :set -XViewPatterns
+
+well behaved on ASCII, at least. 
+ 
+-}
 module Commands.Munging where
 
 import Data.List.Split
@@ -7,98 +15,114 @@ import Data.Char
 import Data.List
 import Data.Monoid
  -- TODO intersperse not intercalate?
+-- TODO handle acronyms (i.e. adjacent uppercase ) correctly 
 
 
-{- $setup
 
->>> :set -XViewPatterns
+-- ================================================================ --
+
+{-| >>> upper "upper"
+"UPPER"
 
 -}
+upper :: String -> String
+upper = fmap toUpper
 
--- |
---
--- >>> squeezeCase ["squeeze","case"]
--- "squeezecase"
---
+{-| >>> lower "LOWER"
+"lower"
+
+-}
+lower :: String -> String
+lower = fmap toLower
+
+{-| 
+
+>>> capitalize "capitalize"
+"Capitalize"
+
+>>> capitalize "CAPITALIZE"
+"Capitalize"
+
+~ @titlecase@
+
+-}
+capitalize :: String -> String
+capitalize [] = []
+capitalize (x:xs) = toUpper x : lower xs
+
+{-| >>> uncapitalize "UNCAPITALIZE"
+"uNCAPITALIZE"
+
+-}
+uncapitalize :: String -> String
+uncapitalize [] = []
+uncapitalize (x:xs) = toLower x : xs
+
+
+
+-- ================================================================ --
+
+{-| >>> squeezeCase ["squeeze","case"]
+"squeezecase"
+
+-}
 squeezeCase :: [String] -> String
-squeezeCase = intercalate ""
+squeezeCase = delimit ""
 
--- |
---
--- >>> spaceCase ["space","case"]
--- "space case"
---
-spaceCase :: [String] -> String
-spaceCase = intercalate " "
+{-| 
 
--- |
--- >>> snakeCase ["snake","case"]
--- "snake_case"
---
-snakeCase :: [String] -> String
-snakeCase = intercalate "_"
+>>> dashCase ["dash","case"]
+"dash-case"
 
--- |
--- >>> dashCase ["dash","case"]
--- "dash-case"
---
+-}
 dashCase :: [String] -> String
-dashCase = intercalate "-"
+dashCase = delimit "-"
 
--- |
--- >>> fileCase ["file","case"]
--- "file/case"
---
+{-| >>> spaceCase ["space","case"]
+"space case"
+
+-}
+spaceCase :: [String] -> String
+spaceCase = delimit " "
+
+{-| >>> snakeCase ["snake","case"]
+"snake_case"
+
+-}
+snakeCase :: [String] -> String
+snakeCase = delimit "_"
+
+{-| >>> fileCase ["file","case"]
+"file/case"
+
+-}
 fileCase :: [String] -> String
-fileCase = intercalate "/"
+fileCase = delimit "/"
 
--- |
--- >>> camelCase ["camel","case"]
--- "camelCase"
---
+{-| 
+
+-}
+delimit :: String -> [String] -> String
+delimit = intercalate 
+
+{-| >>> camelCase ["camel","case"]
+"camelCase"
+
+-}
 camelCase :: [String] -> String
 camelCase [] = ""
 camelCase (word:words) = lower word <> classCase words
 
--- |
--- >>> classCase ["class","case"]
--- "ClassCase"
---
+{-| >>> classCase ["class","case"]
+"ClassCase"
+
+-}
 classCase :: [String] -> String
 classCase = fmap capitalize >>> squeezeCase
 
 
 
--- |
--- >>> upper "upper"
--- "UPPER"
---
-upper :: String -> String
-upper = fmap toUpper
-
--- |
--- >>> lower "LOWER"
--- "lower"
---
-lower :: String -> String
-lower = fmap toLower
-
--- |
--- >>> capitalize "CAPITALIZE"
--- "Capitalize"
---
--- ~ @titlecase@
-capitalize :: String -> String
-capitalize [] = []
-capitalize (x:xs) = toUpper x : lower xs
-
--- |
--- >>> uncapitalize "UNCAPITALIZE"
--- "uNCAPITALIZE"
---
-uncapitalize :: String -> String
-uncapitalize [] = []
-uncapitalize (x:xs) = toLower x : xs
+-- ================================================================ --
 
 {-| 
 
@@ -108,13 +132,107 @@ uncapitalize (x:xs) = toLower x : xs
 >>> unCamelCase ""
 []
 
-prop> \(uncapitalize -> x) -> (camelCase . unCamelCase) x == x
+>>> (camelCase . unCamelCase) "unCamelCase"
+"unCamelCase"
+
+>>> (unCamelCase . camelCase) ["un","camel","case"] 
+["un","camel","case"]
+
+>>> unCamelCase "mtaBidOptimizationUI"
+["mta","bid","optimization","ui"]
+
+prop> \(uncapitalize -> s) -> (camelCase . unCamelCase) s == s -- TODO 
 
 -}
 unCamelCase :: String -> [String]
-unCamelCase = fmap lower . (split . dropInitBlank . keepDelimsL . whenElt) isUpper
+unCamelCase = keepAcronyms . split (dropInitBlank . keepDelimsL . whenElt $ isUpper)
+ where
 
+ keepAcronyms
+  = fmap lower
+  . fmap concat
+  . groupBy isSingleLetter  
+
+ isSingleLetter :: String -> String -> Bool
+ isSingleLetter = (\x y -> length x == 1 && length y == 1)
+
+{-| >>> unClassCase "MTABidOptimizationUI"
+["mta","bid","optimization","ui"]
+
+prop> \(isClassCase -> s) -> (classCase . unClassCase) s == s -- TODO 
+
+-}
+unClassCase :: String -> [String]
+unClassCase = unCamelCase
+
+
+{-| 
+
+>>> unDashCase "un-dash-case"
+["un","dash","case"]
+
+>>> unDashCase "notDashCase"
+["notDashCase"]
+
+>>> unDashCase ""
+[]
+
+prop> \(isDashCase -> s) -> (dashCase . unDashCase) s == s -- TODO 
+
+-}
+unDashCase :: String -> [String]
+unDashCase = unDelimit "-" 
+
+{-| 
+
+-}
+unSpaceCase :: String -> [String]
+unSpaceCase = unDelimit " " 
+
+{-| 
+
+-}
+unSnakeCase :: String -> [String]
+unSnakeCase = unDelimit "_" 
+
+{-| 
+
+-}
+unFileCase :: String -> [String]
+unFileCase = unDelimit "/" 
+
+{-| >>> unDelimit "::" "::global::System.Console.WriteLine" 
+["global","System.Console.WriteLine"]
+
+>>> ["::global::System.Console.WriteLine"] >>= (unClassCase >=> unDelimit "." >=> unDelimit "::")
+["global","system","console","write","line"]
+
+(unClassCase <=< (unDelimit ".") <=< (unDelimit "::")) =<< ["::global::System.Console.WriteLine"]
+["global","system","console","write","line"]
+
+-}
+unDelimit :: String -> String -> [String]
+unDelimit d = split (dropBlanks . dropDelims $ onSublist d)
+
+
+
+-- ================================================================ --
+
+{-| >>> overCamelCase id "overCamelCase"
+"over camel case"
+
+>>> overCamelCase ((:[]) . (!!1)) "overCamelCase"
+"camel"
+
+-}
 overCamelCase :: ([String] -> [String]) -> (String -> String)
 overCamelCase f = (intercalate " ") . f . unCamelCase
 -- overCamelCase = dimap (intercalate " ") unCamelCase
+
+{-| >>> toCamelCase "to camel case" 
+"toCamelCase"
+
+-}
+toCamelCase :: String -> String
+toCamelCase = words >>> camelCase
 

e59d9cde64f3736cd1df6f94b1562a94e60a42a4
Spiros Boosalis <samboosalis@gmail.com>
2016-05-29 14:54:10 +0300

    stack / new workflow-_ pkg

--- sources/Commands/Sugar/Keys.hs
+++ sources/Commands/Sugar/Keys.hs
@@ -1,5 +1,5 @@
 {-# LANGUAGE RecursiveDo, PatternSynonyms, FlexibleContexts                                 #-}
-module Commands.Sugar.Keys where  
+module Commands.Sugar.Keys where
 import           Commands.Extra
 import           Commands.Backends.OSX
 import           Commands.Parsers.Earley (EarleyEither,fullParsesE)
@@ -12,17 +12,17 @@ import           Control.Applicative
 import           Data.Char hiding(Control)
 
 
-{- | parse a string, execute it as a keyboard shortcut 
+{- | parse a string, execute it as a keyboard shortcut
 
 @press = 'runKeyRiff' . 'kbd'@
 
-warning: partial function 
+warning: partial function
 -}
-press :: (MonadWorkflow m) => String -> m () 
+press :: (MonadWorkflow m) => String -> m ()
 press = runKeyRiff . kbd
 
--- | execute a keyboard shortcut 
-runKeyRiff :: (MonadWorkflow m) => KeyRiff -> m () 
+-- | execute a keyboard shortcut
+runKeyRiff :: (MonadWorkflow m) => KeyRiff -> m ()
 runKeyRiff = traverse_ (\(KeyChord mods k) -> sendKeyChord mods k)
 
 {- | a grammar for Emacs-like keybindings syntax, like elisp @kbd@ (<http://www.gnu.org/software/emacs/manual/html_node/elisp/Key-Sequences.html>).
@@ -51,7 +51,7 @@ warning: partial function.
 kbd :: [Char] -> KeyRiff
 kbd cs = either (error.(("(Commands.Sugar.Keys.kbd "++(show cs)++")") ++).show) NonEmpty.head (safeKbd cs)
 
--- | like @kbd@, but a total function 
+-- | like @kbd@, but a total function
 safeKbd :: [Char] -> EarleyEither String Char (NonEmpty KeyRiff)
 safeKbd cs = fullParsesE gKeychords (strip cs)
 -- bimapEither toException .
@@ -63,10 +63,10 @@ safeKbd cs = fullParsesE gKeychords (strip cs)
 
 {- | a grammar for Emacs-like keybindings syntax.
 
-follows (a subset of) <http://emacswiki.org/emacs/EmacsKeyNotation Emacs keybinding syntax>. some differences: 
+follows (a subset of) <http://emacswiki.org/emacs/EmacsKeyNotation Emacs keybinding syntax>. some differences:
 
-* doesn't interpret uppercase non-modifier letters as shifted alphanumerics, for disambiguation. 
-* e.g. use @"M-S-a"@, not @"M-A"@. non-alphanumerics are in angle brackets. 
+* doesn't interpret uppercase non-modifier letters as shifted alphanumerics, for disambiguation.
+* e.g. use @"M-S-a"@, not @"M-A"@. non-alphanumerics are in angle brackets.
 * e.g. @"C-<tab>"@, not @"C-TAB"@. see the source (the parser combinators are readable) for other differences.
 
 -}
@@ -88,44 +88,44 @@ gKeychords = mdo
 
  pKey       <- E.rule$ empty
 
-  <|> NoMod SpaceKey                             <$  E.word "<spc>"
-  <|> NoMod TabKey                               <$  E.word "<tab>"
-  <|> NoMod ReturnKey                            <$  E.word "<ret>"
-  <|> NoMod DeleteKey                            <$  E.word "<del>"
-  <|> NoMod EscapeKey                            <$  E.word "<esc>"
+  <|> SimpleKeyChord SpaceKey                             <$  E.word "<spc>"
+  <|> SimpleKeyChord TabKey                               <$  E.word "<tab>"
+  <|> SimpleKeyChord ReturnKey                            <$  E.word "<ret>"
+  <|> SimpleKeyChord DeleteKey                            <$  E.word "<del>"
+  <|> SimpleKeyChord EscapeKey                            <$  E.word "<esc>"
 
-  <|> NoMod UpArrowKey     <$ E.word  "<up>"
-  <|> NoMod DownArrowKey   <$ E.word  "<down>" 
-  <|> NoMod LeftArrowKey   <$ E.word  "<left>" 
-  <|> NoMod RightArrowKey  <$ E.word  "<right>" 
+  <|> SimpleKeyChord UpArrowKey     <$ E.word  "<up>"
+  <|> SimpleKeyChord DownArrowKey   <$ E.word  "<down>"
+  <|> SimpleKeyChord LeftArrowKey   <$ E.word  "<left>"
+  <|> SimpleKeyChord RightArrowKey  <$ E.word  "<right>"
 
-  <|> NoMod F1Key <$ E.word  "<f1>"
-  <|> NoMod F2Key <$ E.word  "<f2>"
-  <|> NoMod F3Key <$ E.word  "<f3>"
-  <|> NoMod F4Key <$ E.word  "<f4>"
-  <|> NoMod F5Key <$ E.word  "<f5>"
-  <|> NoMod F6Key <$ E.word  "<f6>"
-  <|> NoMod F7Key <$ E.word  "<f7>"
-  <|> NoMod F8Key <$ E.word  "<f8>"
-  <|> NoMod F9Key <$ E.word  "<f9>"
-  <|> NoMod F10Key <$ E.word  "<f10>"
-  <|> NoMod F11Key <$ E.word  "<f11>"
-  <|> NoMod F12Key <$ E.word  "<f12>"
-  <|> NoMod F13Key <$ E.word  "<f13>"
-  <|> NoMod F14Key <$ E.word  "<f14>"
-  <|> NoMod F15Key <$ E.word  "<f15>"
-  <|> NoMod F16Key <$ E.word  "<f16>"
-  <|> NoMod F17Key <$ E.word  "<f17>"
-  <|> NoMod F18Key <$ E.word  "<f18>"
-  <|> NoMod F19Key <$ E.word  "<f19>"
-  <|> NoMod F20Key <$ E.word  "<f20>"
+  <|> SimpleKeyChord F1Key <$ E.word  "<f1>"
+  <|> SimpleKeyChord F2Key <$ E.word  "<f2>"
+  <|> SimpleKeyChord F3Key <$ E.word  "<f3>"
+  <|> SimpleKeyChord F4Key <$ E.word  "<f4>"
+  <|> SimpleKeyChord F5Key <$ E.word  "<f5>"
+  <|> SimpleKeyChord F6Key <$ E.word  "<f6>"
+  <|> SimpleKeyChord F7Key <$ E.word  "<f7>"
+  <|> SimpleKeyChord F8Key <$ E.word  "<f8>"
+  <|> SimpleKeyChord F9Key <$ E.word  "<f9>"
+  <|> SimpleKeyChord F10Key <$ E.word  "<f10>"
+  <|> SimpleKeyChord F11Key <$ E.word  "<f11>"
+  <|> SimpleKeyChord F12Key <$ E.word  "<f12>"
+  <|> SimpleKeyChord F13Key <$ E.word  "<f13>"
+  <|> SimpleKeyChord F14Key <$ E.word  "<f14>"
+  <|> SimpleKeyChord F15Key <$ E.word  "<f15>"
+  <|> SimpleKeyChord F16Key <$ E.word  "<f16>"
+  <|> SimpleKeyChord F17Key <$ E.word  "<f17>"
+  <|> SimpleKeyChord F18Key <$ E.word  "<f18>"
+  <|> SimpleKeyChord F19Key <$ E.word  "<f19>"
+  <|> SimpleKeyChord F20Key <$ E.word  "<f20>"
 
   <|> (either __BUG__ id . digit2keypress . read . (:[])) <$> E.satisfy ((&&) <$> isDigit <*> isAscii)
       -- read and digit2keypress are safe because: isDigit returns number characters between zero and nine inclusive
   <|> (either __BUG__ id . char2keypress) <$> E.satisfy ((&&) <$> (not.isAlphaNum) <*> isAscii)
-      -- must be last 
-  <|> (either __BUG__ id . char2keypress) <$> E.satisfy ((&&) <$> isLower <*> isAscii) -- TODO replace pure bug with effectfull empty 
-      -- must be last 
+      -- must be last
+  <|> (either __BUG__ id . char2keypress) <$> E.satisfy ((&&) <$> isLower <*> isAscii) -- TODO replace pure bug with effectfull empty
+      -- must be last
 
   E.<?> "key"
 
@@ -136,4 +136,3 @@ gKeychords = mdo
  lAppendModifiers ms (ms', k) = KeyChord (ms ++ ms') k
 
 -- TODO a numerate all keybindings with one modifier and one non-modifier key, it's for documentation
-

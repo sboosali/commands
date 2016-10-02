01d45c6858676b24ff39e1436bd35be6f1e5ec93
Sam Boosalis <samboosalis@gmail.com>
2016-10-03 00:17:30 +0300

    munge unless ignore

--- sources/Commands/Servers/Simple.hs
+++ sources/Commands/Servers/Simple.hs
@@ -1,3 +1,5 @@
+
+
 -- |
 module Commands.Servers.Simple
  ( module Commands.Servers.Simple
@@ -5,5 +7,40 @@ module Commands.Servers.Simple
  ) where
 import Commands.Servers.Simple.Types
 
+import qualified Workflow.Core as W
+
+import qualified Network.Wai                    as Wai
+import qualified Network.Wai.Handler.Warp       as Wai
+import Servant
+
+import Control.Monad (unless)
+
+import Prelude.Spiros
+import Prelude()
+
+{-|
+
+e.g.
+
+@
+import Workflow.<platform>
+runSimpleServer $ defaultSettings runWorkflowT
+@
+
+-}
 main = do
   print "Commands.Servers.Simple"
+
+runSimpleServer :: Settings -> IO ()
+runSimpleServer settings@Settings{..} = Wai.run port (serve recognitionAPI (handlers settings))
+
+handlers :: Settings -> Server RecognitionAPI
+handlers = handleRecognition
+-- type family ServerT (layout :: k) (m :: * -> *) :: *
+
+handleRecognition :: Settings -> RecognitionHandler
+handleRecognition Settings{munge, ignore, exec= W.ExecuteWorkflow exec} ws = exec $ do
+  unless (ignore ws) $ do
+      W.sendText (munge ws)
+
+-- _500 :: String -> ServantErr _500 s = err500{ errBody = BS.pack e }
--- sources/Commands/Servers/Simple/Types.hs
+++ sources/Commands/Servers/Simple/Types.hs
@@ -1,6 +1,79 @@
 {-# LANGUAGE TemplateHaskell, DeriveAnyClass #-}
-{-# LANGUAGE DataKinds, TypeOperators #-}
+{-# LANGUAGE DataKinds, TypeOperators, KindSignatures #-}
 {-|
 
 -}
 module Commands.Servers.Simple.Types where
+
+import qualified Workflow.Core as W
+
+import Servant
+import           Data.Text.Lazy                        (Text)
+import           Control.Monad.Trans.Except            (ExceptT)
+
+import GHC.TypeLits (Symbol)
+import Data.Char(toLower)
+
+import Prelude.Spiros
+import Prelude()
+
+type RecognitionHandler = Recognition -> Response ()
+
+{-| The server's response.
+
+-}
+type Response = ExceptT ServantErr IO
+
+{-| signature for a simple foreign function, via JSON and HTTP.
+
+-}
+type PostAPI (s :: Symbol) (a :: *) (b :: *)
+ =  s
+ :> ReqBody '[JSON] a
+ :> Post    '[JSON] b
+
+{- | the API for a successful recognition.
+
+@
+POST /recognition
+@
+
+e.g.
+
+@
+$ export PORT=8666
+$ curl  -X POST  -H "Content-Type: application/json"  -d '["some","words"]'  "http://localhost:$PORT/recognition/"
+$ python -c 'import sys,os,json,urllib2; print (urllib2.urlopen(urllib2.Request("http://localhost:"+os.environ["PORT"]+"/recognition/", json.dumps(["some","words with spaces"]), {"Content-Type": "application/json"})).readline())'
+@
+
+-}
+type RecognitionAPI = PostAPI "recognition" Recognition ()
+
+{- | a successful recognition of an utterance.
+
+>>> 'decode' "[\"hello\",\"world\"]" :: Maybe Recognition
+Just ["hello","world"]
+
+-}
+type Recognition = [String]
+
+{-| Platform-independent settings for the server.
+
+-}
+data Settings = Settings
+ { munge                :: [String] -> String
+ , ignore               :: [String] -> Bool
+ , exec                 :: W.ExecuteWorkflow
+ , port                 :: Int
+ }
+
+defaultSettings :: W.ExecuteWorkflow -> Settings
+defaultSettings exec = Settings{..}
+ where
+ munge = unwords > fmap toLower > (++ " ")
+ ignore = unwords > (`elem` noise)
+ port  = 8888
+ noise = ["the","will","if","him","that","a","she","and"]
+
+recognitionAPI :: Proxy RecognitionAPI
+recognitionAPI = Proxy

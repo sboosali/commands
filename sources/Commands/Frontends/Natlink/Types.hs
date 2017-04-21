1f54c202426f79bf44b89bd0b0e7c4a67d2013a8
Spiros Boosalis <samboosalis@gmail.com>
2017-04-21 00:50:57 -0700

    [bad] merge

--- sources/Commands/Frontends/Natlink/Types.hs
+++ sources/Commands/Frontends/Natlink/Types.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE GeneralizedNewtypeDeriving, StandaloneDeriving, TemplateHaskell #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving, StandaloneDeriving, TemplateHaskell, PatternSynonyms #-}
 
 {-| the speech engine API, as exposed by
 <https://github.com/sboosali/NatLink/blob/master/NatlinkSource/natlink.txt#L106 Natlink>
@@ -10,22 +10,35 @@ TODO websockets + setTimerCallback ?
 -}
 module Commands.Frontends.Natlink.Types where
 import Commands.Frontends.Dragon13.Extra
+import Commands.Frontends.Dragon13.Types (DNSGrammar(..))
+import Commands.Frontends.Dragon13.Text
 
 import Control.Lens(makeLenses,makePrisms)
 import           Data.Aeson (ToJSON,FromJSON) --TODO rm
 
---import Prelude.Spiros
---import Prelude()
+import Prelude.Spiros
+import Prelude()
+
+--------------------------------------------------------------------------------
 
 {-|
+
 -}
+newtype DNSBuffer = DNSBuffer String
 
 {-|
+
 -}
-newtype Recognition = Recognition [DNSWord]
- deriving (Show,Read,Eq,Ord,Generic,Data,NFData,Hashable,Semigroup,Monoid)
+newtype Recognition = Recognition [DNSToken]
+ deriving (Show,Read,Eq,Ord,Generic,Data,NFData,Hashable,ToJSON,FromJSON,Semigroup,Monoid)
+
+-- | only the utterance ('dnsPronounced'), without metadata
+rawRecognition :: Recognition -> [String]
+rawRecognition = toListOf (_Recognition.dnsPronounced)
 
 {-| the "leaves" of the grammar.
+
+Returned by Dragon NaturallySpeaking when an utterance is recognized.
 
 given @DNSPronounced written spoken@, the speech recognition engine
 recognizes @spoken@ and transcribes it as @written@.
@@ -33,7 +46,7 @@ recognizes @spoken@ and transcribes it as @written@.
 e.g.
 
 @
-DNSWord
+DNSToken
  { dnsPronounced="a"
  , dnsWritten="A"
  , dnsCategory="letter"
@@ -41,20 +54,51 @@ DNSWord
 @
 
 -}
-data DNSWord = DNSWord
- { dnsPronounced :: String
- , dnsWritten    :: String
- , dnsCategory   :: String
+data DNSToken = DNSToken
+ { _dnsPronounced :: String
+ , _dnsWritten    :: String
+ , _dnsCategory   :: String
  }
  deriving (Show,Read,Eq,Ord,Data,Generic)
-instance NFData   DNSWord
-instance Hashable DNSWord
+instance NFData   DNSToken
+instance Hashable DNSToken
+instance ToJSON   DNSToken
+instance FromJSON DNSToken
+
+toDNSToken :: String -> Maybe DNSToken --TODO
+toDNSToken s = if s & (validDNSToken > isJust) then Just DNSToken{..} else Nothing
+ where
+ _dnsPronounced = s
+ _dnsWritten = ""
+ _dnsCategory = ""
+
+validDNSToken :: String -> Maybe String --TODO
+validDNSToken s = Just s
+
+{-| the "leaves" of the grammar.
+
+Provided to Dragon NaturallySpeaking in the grammar that is loaded.
+
+Validated by 'validateDNSWord'
+
+-}
+data DNSWord = DNSWord String
+
+validateDNSWord :: String -> Maybe DNSWord
+validateDNSWord s = Just s --TODO
+
+--------------------------------------------------------------------------------
+
+{-|
+
+-}
+pattern MAXIMUM_ALTERNATIVE_RECOGNITIONS = 10
 
 {-| <http://www.nuance.com/naturallyspeaking/customer-portal/documentation/userguide/chapter7/ug_chapter7_switch_recognition_mode.asp>
 
 -}
 data DNSMode
- = NormalMode
+ = NormalMode -- ^ Both Dictation and Commands
  | DictationMode
  | CommandMode
  | NumbersMode
@@ -78,9 +122,241 @@ instance Hashable MicrophoneState
 instance ToJSON   MicrophoneState
 instance FromJSON MicrophoneState
 
-data GrammarObject = GrammarObject Int
+data GrammarObject = GrammarObject_ { getGrammarObject :: Int }
+unsafeGrammarObject = GrammarObject
 
-data ResultsObject = ResultsObject Int
+data ResultsObject = ResultsObject_ { getResultsObject :: Int }
+unsafeResultsObject = ResultsObject
+
+newtype PythonExpression = PythonExpression { getPythonExpression :: String }
+
+newtype DragonScriptExpression = DragonScriptExpression { getDragonScriptExpression :: String }
+
+{-|
+
+Training can fail of the transcription is not close enough to the utterance.
+
+-}
+data CorrectionStatus = CorrectionSuccess | CorrectionHeterophonic | CorrectionInvalidWord
+ deriving (Show,Read,Eq,Ord,Enum,Bounded,Data,Generic)
+instance NFData   MicrophoneState
+instance Hashable MicrophoneState
+instance ToJSON   MicrophoneState
+instance FromJSON MicrophoneState
+
+-- | True is "good", False is "bad"
+booleanCorrectionStatus :: CorrectionStatus -> Bool
+booleanCorrectionStatus = \case
+  CorrectionSuccess      -> True
+  CorrectionHeterophonic -> False
+  CorrectionInvalidWord  -> False
+
+{-|
+
+Audio data in wave format.
+
+11.025 * 4000 = 441000
+
+
+-}
+data Utterance = Utterance
+  { _utteranceData :: () --TODO
+  }
+
+{-|
+
+        Can raise InvalidWord if the grammar contains an invalid word.
+        Can raise BadGrammar if the grammar has syntax errors, or if it's is too complex
+
+-}
+data GrammarLoadStatus = GrammarLoadSuccess | GrammarLoadBadGrammar | GrammarLoadInvalidWord
+
+-- | True is "good", False is "bad"
+booleanGrammarLoadStatus :: GrammarLoadStatus -> Bool
+booleanGrammarLoadStatus = \case
+  GrammarLoadSuccess      -> True
+  GrammarLoadBadGrammar   -> False
+  GrammarLoadInvalidWord  -> False
+
+--------------------------------------------------------------------------------
+
+{-| Initialization/configuration properties shared by all grammars.
+
+-}
+data GrammarProperties = GrammarProperties
+  { _grammarStatus            :: Status
+  , _grammarExclusivity       :: Exclusivity
+  , _grammarShouldEavesdrop   :: ShouldEavesdrop
+  , _grammarShouldHypothesize :: ShouldHypothesize
+--  , grammar ::
+  }
+
+{-|
+
+-}
+defaultGrammarProperties :: GrammarProperties
+defaultGrammarProperties = fastGrammarProperties
+
+{-|
+
+-}
+narcissisticGrammarProperties :: GrammarProperties
+narcissisticGrammarProperties = GrammarProperties{..}
+ where
+ _grammarStatus = Enabled
+ _grammarExclusivity = Exclusive
+ _grammarShouldEavesdrop = YesEavesdrop
+ _grammarShouldHypothesize = YesHypothesize
+ -- grammar =
+
+{-|
+
+-}
+fastGrammarProperties :: GrammarProperties
+fastGrammarProperties = GrammarProperties{..}
+ where
+ _grammarStatus = Enabled
+ _grammarExclusivity = Inclusive
+ _grammarShouldEavesdrop = NoEavesdrop
+ _grammarShouldHypothesize = NoHypothesize
+ -- grammar =
+
+-- | 'Bool'-like. (A grammar can be loaded but disabled.)
+data Status = Enabled | Disabled
+ deriving (Show,Read,Eq,Ord,Bounded,Enum,Data,Generic) -- TODO Monoid All?
+
+-- | 'Bool'-like. Whether the grammar is exclusive, i.e. it disables all other grammars.
+data Exclusivity = Inclusive | Exclusive -- TODO Monoid Any?
+ deriving (Show,Read,Eq,Ord,Enum,Bounded,Data,Generic)
+
+-- | 'Bool'-like. Should the grammar listen to other grammars' recognitions' callbacks.
+data ShouldEavesdrop = YesEavesdrop | NoEavesdrop
+ deriving (Show,Read,Eq,Ord,Enum,Bounded,Data,Generic)
+
+-- | 'Bool'-like. Should the grammar listen to each hypothesis's callback.
+data ShouldHypothesize = YesHypothesize | NoHypothesize
+ deriving (Show,Read,Eq,Ord,Enum,Bounded,Data,Generic)
+
+--------------------------------------------------------------------------------
+
+{-|
+
+-}
+data ControlGrammar = ControlGrammar
+  { _controlProperties    :: GrammarProperties
+  , _controlConfiguration :: DNSGrammar DNSInfo DNSText DNSName
+  } deriving (Show,Eq,Ord)
+
+-- data ControlConfiguration = ControlConfiguration
+--   { _control ::
+--   }
+
+{-|
+
+-}
+data ControlResults = ControlResults
+  { _controlRecognition :: Recognition
+--  , _control ::
+  }
+
+--------------------------------------------------------------------------------
+
+{-|
+
+Whenever the selection buffer is updated, Dragon NaturallySpeaking
+implicitly constructs a grammar with this production:
+
+@
+<'selectionSelectWords'> <subsequence> [ <'selectionThroughWords'> <subsequence> ]
+@
+
+where @<selectionSelectWords>@ and @<selectionThroughWords>@ are lists, and
+@<subsequence>@ is any subsequence in the selection buffer.
+
+Manually constructing @<subsequence>@, in a (non-selection) 'Grammar', would be
+extremely inefficient, as it grows quadradically in the size of the buffer.
+
+also see @MacroSystem/core/natlinkutils.py#L755@
+
+https://github.com/sboosali/NatLink/blob/9545436181f23652224041afa2035f12fa60d949/MacroSystem/core/natlinkutils.py#L755
+
+"A select XYZ grammar is a special grammar which
+recognizes an utterance of the form "<verb> <text> [ through <text> ]"
+where <verb> is specified and <text> is an arbitrary sequence of words in
+a specified text buffer."
+
+-}
+data SelectionGrammar = SelectionGrammar
+  -- { selectionStatus :: Status
+  -- , selectionExclusivity :: Exclusivity
+  -- , selectionShouldEavesdrop :: ShouldEavesdrop
+  -- , selectionShouldHypothesize :: ShouldHypothesize
+  { _selectionProperties    :: GrammarProperties
+  , _selectionConfiguration :: SelectionConfiguration
+--  , selection ::
+  }
+
+{-|
+
+-}
+data SelectionConfiguration = SelectionConfiguration
+  { _selectionSelectWords  :: [String]
+  , _selectionThroughWords :: [String]
+--  , selection ::
+  }
+
+{-|
+
+-}
+defaultSelectionGrammar :: SelectionGrammar
+defaultSelectionGrammar = SelectionGrammar{..}
+ where
+ _selectionProperties = defaultGrammarProperties
+ _selectionSelectWords = ["select","correct","insert before","insert after","capitalize"] -- TODO Reproduce all NaturallySpeaking's Selection commands
+ _selectionThroughWords = ["through","until"] -- TODO non-words, so they dont conflict with the buffer itself
+
+newtype SelectionGrammarId = SelectionGrammarId Int
+unsafeSelectionGrammarId = SelectionGrammarId
+
+data SelectionSettingStatus = SelectionSettingSuccess
+
+{-|
+
+see @ResultsObject.getSelectInfo()@
+
+-}
+data SelectionResultsObject = SelectionResultsObject
+ { _sroRecognition :: Recognition -- ^ A subsequence of the selection buffer
+ , _sroOffset      :: Natural     -- ^ Bounded by the length of the selection buffer -- TODO Necessary?
+ }
+
+--------------------------------------------------------------------------------
+
+class MonadNatlink m where
+
+ executeScript :: DragonScriptExpression -> m ()  -- ^ [documentation](https://github.com/sboosali/NatLink/blob/master/NatlinkSource/natlink.txt#L190)
+ recognitionMimic :: Recognition -> m ()  -- ^ [documentation](https://github.com/sboosali/NatLink/blob/master/NatlinkSource/natlink.txt#L209)
+
+ setMicrophoneState :: MicrophoneState -> m ()  -- ^ [documentation](https://github.com/sboosali/NatLink/blob/master/NatlinkSource/natlink.txt#L178)
+ getMicrophoneState :: m (MicrophoneState)  -- ^ [documentation](https://github.com/sboosali/NatLink/blob/master/NatlinkSource/natlink.txt#L182)
+
+ setTimerCallback :: DragonScriptExpression -> m ()  -- ^ [documentation](https://github.com/sboosali/NatLink/blob/master/NatlinkSource/natlink.txt#L279)
+
+ loadGrammarObject :: () -> m ()  -- ^ [documentation](https://github.com/sboosali/NatLink/blob/master/NatlinkSource/natlink.txt#L574)
+ activateGrammarRule :: () -> m ()  -- ^ [documentation](https://github.com/sboosali/NatLink/blob/master/NatlinkSource/natlink.txt#L601)
+ dectivateGrammarRule :: () -> m ()  -- ^ [documentation](https://github.com/sboosali/NatLink/blob/master/NatlinkSource/natlink.txt#L614)
+ setExclusiveGrammar :: () -> m ()  -- ^ [documentation](https://github.com/sboosali/NatLink/blob/master/NatlinkSource/natlink.txt#L619)
+
+-- getResultsObject :: () -> m ()  -- ^ [documentation](https://github.com/sboosali/NatLink/blob/master/NatlinkSource/natlink.txt#L759)
+ getResultsObjectAudio :: (ResultsObject) -> m (RecognitionAudio)  -- ^ [documentation](https://github.com/sboosali/NatLink/blob/master/NatlinkSource/natlink.txt#L753)
+ correctResultsObject :: (ResultsObject) -> Recognition -> m ()  -- ^ [documentation](https://github.com/sboosali/NatLink/blob/master/NatlinkSource/natlink.txt#L740)
+
+ setWordInfo :: () -> m ()  -- ^ [documentation](https://github.com/sboosali/NatLink/blob/master/NatlinkSource/natlink.txt#L466)
+ getWordInfo :: () -> m ()  -- ^ [documentation](https://github.com/sboosali/NatLink/blob/master/NatlinkSource/natlink.txt#L373)
+ deleteWord :: () -> m ()  -- ^ [documentation](https://github.com/sboosali/NatLink/blob/master/NatlinkSource/natlink.txt#L421)
+ addWord :: () -> m ()  -- ^ [documentation](https://github.com/sboosali/NatLink/blob/master/NatlinkSource/natlink.txt#L428)
+
+ setSelection :: SelectionGrammarId -> DNSBuffer -> m SelectionSettingStatus
 
 {-|
 -}
@@ -108,33 +384,25 @@ data NatlinkF k
  | GetResultsObjectAudio  -- ^ [documentation](https://github.com/sboosali/NatLink/blob/master/NatlinkSource/natlink.txt#L753)
  | CorrectResultsObject  -- ^ [documentation](https://github.com/sboosali/NatLink/blob/master/NatlinkSource/natlink.txt#L740)
 
-class MonadNatlink m where
-
- executeScript :: () -> m ()  -- ^ [documentation](https://github.com/sboosali/NatLink/blob/master/NatlinkSource/natlink.txt#L190)
- recognitionMimic :: () -> m ()  -- ^ [documentation](https://github.com/sboosali/NatLink/blob/master/NatlinkSource/natlink.txt#L209)
-
- setMicrophoneState :: () -> m ()  -- ^ [documentation](https://github.com/sboosali/NatLink/blob/master/NatlinkSource/natlink.txt#L178)
- getMicrophoneState :: () -> m ()  -- ^ [documentation](https://github.com/sboosali/NatLink/blob/master/NatlinkSource/natlink.txt#L182)
-
- setTimerCallback :: {-Python-}String -> m ()  -- ^ [documentation](https://github.com/sboosali/NatLink/blob/master/NatlinkSource/natlink.txt#L279)
-
- setWordInfo :: () -> m ()  -- ^ [documentation](https://github.com/sboosali/NatLink/blob/master/NatlinkSource/natlink.txt#L466)
- getWordInfo :: () -> m ()  -- ^ [documentation](https://github.com/sboosali/NatLink/blob/master/NatlinkSource/natlink.txt#L373)
- deleteWord :: () -> m ()  -- ^ [documentation](https://github.com/sboosali/NatLink/blob/master/NatlinkSource/natlink.txt#L421)
- addWord :: () -> m ()  -- ^ [documentation](https://github.com/sboosali/NatLink/blob/master/NatlinkSource/natlink.txt#L428)
-
- loadGrammarObject :: () -> m ()  -- ^ [documentation](https://github.com/sboosali/NatLink/blob/master/NatlinkSource/natlink.txt#L574)
- activateGrammarRule :: () -> m ()  -- ^ [documentation](https://github.com/sboosali/NatLink/blob/master/NatlinkSource/natlink.txt#L601)
- dectivateGrammarRule :: () -> m ()  -- ^ [documentation](https://github.com/sboosali/NatLink/blob/master/NatlinkSource/natlink.txt#L614)
- setExclusiveGrammar :: () -> m ()  -- ^ [documentation](https://github.com/sboosali/NatLink/blob/master/NatlinkSource/natlink.txt#L619)
-
- getResultsObject :: () -> m ()  -- ^ [documentation](https://github.com/sboosali/NatLink/blob/master/NatlinkSource/natlink.txt#L759)
- getResultsObjectAudio :: () -> m ()  -- ^ [documentation](https://github.com/sboosali/NatLink/blob/master/NatlinkSource/natlink.txt#L753)
- correctResultsObject :: () -> m ()  -- ^ [documentation](https://github.com/sboosali/NatLink/blob/master/NatlinkSource/natlink.txt#L740)
-
 --------------------------------------------------------------------------------
 
 makePrisms ''Recognition
+makePrisms ''DNSBuffer
+makeLenses ''DNSToken
 makeLenses ''DNSWord
+
 makePrisms ''DNSMode
 makePrisms ''MicrophoneState
+
+makeLenses ''GrammarProperties
+makeLenses ''ControlGrammar
+makeLenses ''SelectionGrammar
+makeLenses ''ControlConfiguration
+makeLenses ''SelectionConfiguration
+
+makeLenses ''ControlResults
+makeLenses ''SelectionResults
+
+makePrisms ''RecognitionAudio
+makePrisms ''DragonScriptExpression
+makePrisms ''PythonExpression
